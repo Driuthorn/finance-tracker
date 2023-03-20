53c673cea453e69f3fbe3a96ab9f3ba76e7cf57a
Driuthorn <douglas.amaral@hotmail.com>
2023-03-19 21:10:49 -0300

    feat: Start creating content of home screen

--- src/presentation/screens/home/home.tsx
+++ src/presentation/screens/home/home.tsx
@@ -5,8 +5,32 @@ import { useState } from "react"
 import { createStyles } from "./styles"
 import { View, Text } from 'react-native'
 
+const getWeekDay = (day: number) => {
+  var weekDays = ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado',]
+
+  return weekDays[day];
+}
+
+const getMonth = (month: number) => {
+  var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+  return months[month];
+}
+
+const getActualDate = () => {
+  const newDate = new Date();
+
+  return {
+    day: newDate.getDate(),
+    month: getMonth(newDate.getMonth()),
+    weekDay: getWeekDay(newDate.getDay()),
+    year: newDate.getFullYear()
+  }
+}
+
 const initialState = {
-  userInfos: ''
+  userInfos: '',
+  actualDate: getActualDate()
 }
 
 const initialLoadingState = {
@@ -38,7 +62,18 @@ export const HomeScreen = ({
   
   return (
     <View style={Styles.container}>
-      <Text>TESTE</Text>
+      <View style={Styles.headerContent}>
+        <Text style={Styles.headerText}>Dia {state.actualDate.day} {state.actualDate.weekDay},</Text>
+        <Text style={Styles.headerText}>{state.actualDate.month} de {state.actualDate.year}</Text>
+      </View>
+      <View style={Styles.content}>
+        <View style={[Styles.contentBox, Styles.contentBoxBorder, Styles.greenContentBox]}>
+
+        </View>
+        <View style={[Styles.contentBox, Styles.contentBoxBorder, Styles.redContentBox]}></View>
+
+        <View style={[Styles.contentBox, Styles.contentBoxBorderless, Styles.contentBoxBorderless]}></View>
+      </View>
     </View>
   )
 }
--- src/presentation/screens/home/styles.ts
+++ src/presentation/screens/home/styles.ts
@@ -6,7 +6,35 @@ export const createStyles = (theme: Theme) =>
     container: {
       flex: 1,
     },
+    headerContent: {
+      padding: 25,
+    },
+    headerText: {
+      fontSize: theme.fontSize.MEDIUM,
+    },
     content: {
-      flex: 1,
-    }
+      position: 'relative',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    contentBox: {
+      padding: 20,
+      marginBottom: 10,
+      width: '75%',
+      heigh: '500%',
+      backgroundColor: theme.colors.BOX_BACKGROUND_COLOR,
+    },
+    contentBoxBorderless: {
+      borderRadius: 30,
+    },
+    contentBoxBorder: {
+      borderWidth: 5,
+      borderRadius: 30,
+    },
+    greenContentBox: {
+      borderColor: theme.colors.PRIMARY,
+    },
+    redContentBox: {
+      borderColor: theme.colors.ERROR,
+    },
   })
